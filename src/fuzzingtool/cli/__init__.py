9dc4dca4e51bbde4d2524f9066e57969b1d933fe
Wixmix <vitor.borges@computacao.ufla.br>
2021-05-11 17:13:44 -0300

    Inserted __init__.py

--- src/fuzzingtool/cli/__init__.py
+++ src/fuzzingtool/cli/__init__.py
@@ -9,16 +9,3 @@
 #    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 ## https://github.com/NESCAU-UFLA/FuzzingTool
-
-from .BaseFactories import BaseHttpFactory
-from ..conn.requests import *
-
-from importlib import import_module
-
-class HttpFactory(BaseHttpFactory):
-    def requestCreator(requestType, url, **kwargs):
-        Request = import_module(
-            f"fuzzingtool.conn.requests.{requestType}",
-            package=requestType
-        )
-        return getattr(Request, requestType)(url, **kwargs)
