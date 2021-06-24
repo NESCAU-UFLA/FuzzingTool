435ca8538644f015caba40c0e8f351629f97fbe3
Wixmix <vitor.borges@computacao.ufla.br>
2021-06-24 10:32:32 -0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         "Development Status :: 4 - Beta",
         "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
