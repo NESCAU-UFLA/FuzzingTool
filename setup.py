2cbba836365456e0ba01d793779fe9e961a0e015
Wixmix <vitor.borges@computacao.ufla.br>
2021-06-24 12:10:31 -0300

    Added operating system to setup.py classifiers.

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "Development Status :: 4 - Beta",
         "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
