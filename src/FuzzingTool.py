2f9d5d8e028d448d8806384baa272e4bfe332377
VitorOriel <vitorwixmix@gmail.com>
2021-08-24 14:39:05 -0300

    Updated copyright notice.

--- src/FuzzingTool.py
+++ src/FuzzingTool.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright (c) 2021 Vitor Oriel <https://github.com/VitorOriel>
+# Copyright (c) 2020 - present Vitor Oriel <https://github.com/VitorOriel>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
--- src/fuzzingtool/utils/utils.py
+++ src/fuzzingtool/utils/utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Vitor Oriel <https://github.com/VitorOriel>
+# Copyright (c) 2020 - present Vitor Oriel <https://github.com/VitorOriel>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
