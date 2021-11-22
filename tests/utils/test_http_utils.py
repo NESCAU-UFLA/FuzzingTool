2664cfcae605e798bebcaefdb08a5fd222e698c6
VitorOriel <vitorwixmix@gmail.com>
2021-11-22 10:11:43 -0300

    Updated test_utils.

--- tests/utils/test_http_utils.py
+++ tests/utils/test_http_utils.py
@@ -5,40 +5,43 @@ from src.fuzzingtool.utils.http_utils import *
 
 
 class TestHttpUtils(unittest.TestCase):
-    def test_get_url_without_scheme(self):
+    def test_get_url_without_scheme_without_scheme(self):
         return_expected = "test-url.com/"
-        #  Testing with scheme
-        test_url = "https://test-url.com/"
-        returned_data = get_url_without_scheme(test_url)
-        self.assertIsInstance(returned_data, str)
-        self.assertEqual(returned_data, return_expected)
-        #  Testing withour scheme
         test_url = "test-url.com/"
         returned_data = get_url_without_scheme(test_url)
         self.assertIsInstance(returned_data, str)
         self.assertEqual(returned_data, return_expected)
 
-    def test_get_pure_url(self):
+    def test_get_url_without_scheme_with_scheme(self):
+        return_expected = "test-url.com/"
+        test_url = "https://test-url.com/"
+        returned_data = get_url_without_scheme(test_url)
+        self.assertIsInstance(returned_data, str)
+        self.assertEqual(returned_data, return_expected)
+
+    def test_get_pure_url_without_mark(self):
         return_expected = "https://test-url.com/"
-        #  Testing without FUZZING_MARK
         test_url = "https://test-url.com/"
         returned_data = get_pure_url(test_url)
         self.assertIsInstance(returned_data, str)
         self.assertEqual(returned_data, return_expected)
-        #  Testing with FUZZING_MARK
+
+    def test_get_pure_url_with_mark(self):
+        return_expected = "https://test-url.com/"
         test_url = f"https://test-url.com/{FUZZING_MARK}"
         returned_data = get_pure_url(test_url)
         self.assertIsInstance(returned_data, str)
         self.assertEqual(returned_data, return_expected)
-        #  Testing with FUZZING_MARK and dot
+
+    def test_get_pure_url_with_mark_and_dot(self):
+        return_expected = "https://test-url.com/"
         test_url = f"https://{FUZZING_MARK}.test-url.com/"
         returned_data = get_pure_url(test_url)
         self.assertIsInstance(returned_data, str)
         self.assertEqual(returned_data, return_expected)
-    
+
     @patch("src.fuzzingtool.utils.http_utils.get_url_without_scheme")
     def test_get_path(self, mock_get_url_without_scheme):
-        #  Test with root directory on URL
         return_expected = "/"
         test_url = "https://test-url.com/"
         mock_get_url_without_scheme.return_value = "test-url.com/"
@@ -49,19 +52,23 @@ class TestHttpUtils(unittest.TestCase):
         self.assertEqual(returned_data, return_expected)
 
     @patch("src.fuzzingtool.utils.http_utils.get_url_without_scheme")
-    def test_get_host(self, mock_get_url_without_scheme):
+    def test_get_host_without_root_directory(self, mock_get_url_without_scheme):
         return_expected = "test-url.com"
-        #  Test with root directory on URL
-        test_url = "https://test-url.com/"
-        mock_get_url_without_scheme.return_value = "test-url.com/"
+        test_url = "https://test-url.com"
+        mock_get_url_without_scheme.return_value = "test-url.com"
         returned_data = get_host(test_url)
         mock_get_url_without_scheme.assert_called_with(test_url)
         mock_get_url_without_scheme.assert_called_once()
         self.assertIsInstance(returned_data, str)
         self.assertEqual(returned_data, return_expected)
-        #  Test without root directory on URL
-        test_url = "https://test-url.com"
-        mock_get_url_without_scheme.return_value = "test-url.com"
+ 
+    @patch("src.fuzzingtool.utils.http_utils.get_url_without_scheme")
+    def test_get_host_with_root_directory(self, mock_get_url_without_scheme):
+        return_expected = "test-url.com"
+        test_url = "https://test-url.com/"
+        mock_get_url_without_scheme.return_value = "test-url.com/"
         returned_data = get_host(test_url)
+        mock_get_url_without_scheme.assert_called_with(test_url)
+        mock_get_url_without_scheme.assert_called_once()
         self.assertIsInstance(returned_data, str)
         self.assertEqual(returned_data, return_expected)
--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -2,100 +2,121 @@ import unittest
 from unittest.mock import patch
 
 from src.fuzzingtool.utils.utils import *
+from src.fuzzingtool.utils.utils import _get_letter_range, _get_number_range
 from src.fuzzingtool.utils.consts import FUZZING_MARK
 
 
 class TestUtils(unittest.TestCase):
-    def test_get_indexes_to_parse(self):
-        # Test without mark
+    def test_get_indexes_to_parse_without_mark(self):
         return_expected = []
         test_content = "payload"
         returned_data = get_indexes_to_parse(test_content)
         self.assertIsInstance(returned_data, list)
         self.assertEqual(returned_data, return_expected)
-        # Test with only one mark
+
+    def test_get_indexes_to_parse_with_one_mark(self):
         return_expected = [2]
         test_content = f"pa{FUZZING_MARK}yload"
         returned_data = get_indexes_to_parse(test_content)
         self.assertIsInstance(returned_data, list)
         self.assertEqual(returned_data, return_expected)
-        # Test with two marks
+
+    def test_get_indexes_to_parse_with_two_marks(self):
         return_expected = [2, 8]
         test_content = f"pa{FUZZING_MARK}yload{FUZZING_MARK}"
         returned_data = get_indexes_to_parse(test_content)
         self.assertIsInstance(returned_data, list)
         self.assertEqual(returned_data, return_expected)
 
-    def test_split_str_to_list(self):
-        # Test with blank string
+    def test_split_str_to_list_with_blank_string(self):
         return_expected = []
         test_content = ''
         returned_data = split_str_to_list(test_content)
         self.assertIsInstance(returned_data, list)
         self.assertEqual(returned_data, return_expected)
-        # Test string without separator
+
+    def test_split_str_to_list_without_separator(self):
         return_expected = ["payload"]
         test_content = "payload"
         returned_data = split_str_to_list(test_content)
         self.assertIsInstance(returned_data, list)
         self.assertEqual(returned_data, return_expected)
-        # Test string with only one separator
+
+    def test_split_str_to_list_with_one_separator(self):
         return_expected = ["pay", "load"]
         test_content = "pay,load"
         returned_data = split_str_to_list(test_content)
         self.assertIsInstance(returned_data, list)
         self.assertEqual(returned_data, return_expected)
-        # Test string with two separators
+
+    def test_split_str_to_list_with_two_separators(self):
         return_expected = ["pay", "load", '']
         test_content = "pay,load,"
         returned_data = split_str_to_list(test_content)
         self.assertIsInstance(returned_data, list)
         self.assertEqual(returned_data, return_expected)
-        # Test string with one separator and ignorer
+
+    def test_split_str_to_list_with_one_separator_and_ignorer(self):
         return_expected = ["pay,load"]
         test_content = "pay\\,load"
         returned_data = split_str_to_list(test_content)
         self.assertIsInstance(returned_data, list)
         self.assertEqual(returned_data, return_expected)
-        # Test string with two separators and one ignorer
+
+    def test_split_str_to_list_with_two_separators_and_ignorer(self):
         return_expected = ["pay,loa", 'd']
         test_content = "pay\\,loa,d"
         returned_data = split_str_to_list(test_content)
         self.assertIsInstance(returned_data, list)
         self.assertEqual(returned_data, return_expected)
 
-    def test_stringfy_list(self):
-        # Test with empty list
+    def test_stringfy_list_with_empty_list(self):
         return_expected = ''
         test_list = []
         returned_data = stringfy_list(test_list)
         self.assertIsInstance(returned_data, str)
         self.assertEqual(returned_data, return_expected)
-        # Test with not empty list
+
+    def test_stringfy_list_with_filled_list(self):
         return_expected = 'one,two,3'
         test_list = ["one", "two", "3"]
         returned_data = stringfy_list(test_list)
         self.assertIsInstance(returned_data, str)
         self.assertEqual(returned_data, return_expected)
 
-    def test_get_human_length(self):
-        # Test with 0 bytes
+    def test_get_human_length_with_zero_bytes(self):
         return_expected = (0, "B ")
         test_length = 0
         returned_data = get_human_length(test_length)
         self.assertIsInstance(returned_data, tuple)
         self.assertEqual(returned_data, return_expected)
-        # Test with 1025 bytes
+
+    def test_get_human_length_with_float_return(self):
         return_expected = (1.0009765625, "KB")
         test_length = 1025
         returned_data = get_human_length(test_length)
         self.assertIsInstance(returned_data, tuple)
         self.assertEqual(returned_data, return_expected)
-        # Test with 1 GB
+
+    def test_get_human_length_with_one_gb(self):
         return_expected = (1, "GB")
         test_length = 1073741824  # Equals to 1 GB
         returned_data = get_human_length(test_length)
         self.assertIsInstance(returned_data, tuple)
+        self.assertEqual(returned_data, return_expected)
+
+    def test_get_letter_range(self):
+        return_expected = ['a', 'b', 'c', 'd']
+        test_content = ('a', 'd')
+        returned_data = _get_letter_range(*test_content)
+        self.assertIsInstance(returned_data, list)
+        self.assertEqual(returned_data, return_expected)
+
+    def test_get_number_range(self):
+        return_expected = ['0', '1', '2', '3', '4', '5']
+        test_content = ('0', '5')
+        returned_data = _get_number_range(*test_content)
+        self.assertIsInstance(returned_data, list)
         self.assertEqual(returned_data, return_expected)
 
     def test_check_range_list(self):
