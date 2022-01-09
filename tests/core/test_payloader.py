bd33b4fe433d19ffb73e9cdf381909588ff42993
VitorOriel <vitorwixmix@gmail.com>
2022-01-09 20:03:21 -0300

    Added test_encode_manager.

--- tests/core/test_payloader.py
+++ tests/core/test_payloader.py
@@ -2,7 +2,9 @@ import unittest
 from typing import List
 
 from src.fuzzingtool.core.payloader import Payloader, EncodeManager
+from src.fuzzingtool.core.plugins.encoders.hex import Hex
 from src.fuzzingtool.objects.payload import Payload
+from src.fuzzingtool.exceptions.main_exceptions import BadArgumentFormat
 
 
 def assert_payload_list_is_equal(payloads: List[Payload], other_payloads: List[Payload]) -> None:
@@ -14,12 +16,52 @@ def assert_payload_list_is_equal(payloads: List[Payload], other_payloads: List[P
         assert payload.config == other_payloads[i].config
 
 
+class TestEncodeManager(unittest.TestCase):
+    def test_set_regex_with_invalid_regex(self):
+        with self.assertRaises(BadArgumentFormat) as e:
+            EncodeManager().set_regex(r"?([A-Z][a-z]")
+        self.assertEqual(str(e.exception), f"Invalid regex format ?([A-Z][a-z]")
+
+    def test_encode_without_regex(self):
+        return_expected = "746573745f7061796c6f6164"
+        test_payload = "test_payload"
+        returned_encoded_payload = EncodeManager()._encode(Hex(), test_payload)
+        self.assertIsInstance(returned_encoded_payload, str)
+        self.assertEqual(returned_encoded_payload, return_expected)
+
+    def test_encode_with_regex_without_regex_match(self):
+        test_payload = ":test_payload;"
+        return_expected = test_payload
+        test_encode_manager = EncodeManager()
+        test_encode_manager.set_regex("<|>")
+        returned_encoded_payload = test_encode_manager._encode(Hex(), test_payload)
+        self.assertIsInstance(returned_encoded_payload, str)
+        self.assertEqual(returned_encoded_payload, return_expected)
+
+    def test_encode_with_regex_with_regex_match(self):
+        return_expected = "3ctest_payload3e"
+        test_payload = "<test_payload>"
+        test_encode_manager = EncodeManager()
+        test_encode_manager.set_regex("<|>")
+        returned_encoded_payload = test_encode_manager._encode(Hex(), test_payload)
+        self.assertIsInstance(returned_encoded_payload, str)
+        self.assertEqual(returned_encoded_payload, return_expected)
+
+    def test_set_encoder(self):
+        test_payload = "test_payload"
+        return_expected = [Payload(test_payload).with_encoder("746573745f7061796c6f6164", "Hex")]
+        test_encode_manager = EncodeManager()
+        test_encode_manager.set_encoders(([Hex()], []))
+        returned_encoded_payload = test_encode_manager.encode([Payload(test_payload)])
+        self.assertIsInstance(returned_encoded_payload, list)
+        assert_payload_list_is_equal(returned_encoded_payload, return_expected)
+
+
 class TestPayloader(unittest.TestCase):
     def tearDown(self):
         Payloader.prefix = []
         Payloader.suffix = []
         Payloader.case = lambda ajusted_payload: ajusted_payload
-        Payloader.encoder = EncodeManager()
 
     def test_get_customized_payload_without_mutation(self):
         test_payload = "test_payload"
